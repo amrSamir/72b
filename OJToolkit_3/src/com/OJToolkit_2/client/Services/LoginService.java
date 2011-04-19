71a7a0eb75e448867d216bc0a760340521c150e3
amr.9102@gmail.com <amr.9102@gmail.com>
2011-04-19 08:48:23 +0000

    Removed empty lines

--- OJToolkit_3/src/com/OJToolkit_2/client/Services/LoginService.java
+++ OJToolkit_3/src/com/OJToolkit_2/client/Services/LoginService.java
@@ -7,5 +7,4 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 @RemoteServiceRelativePath("login")
 public interface LoginService extends RemoteService {
 	public LoginInfo login(String requestUri);
-	
 }

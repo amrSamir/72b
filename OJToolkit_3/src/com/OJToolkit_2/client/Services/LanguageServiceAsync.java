79cde0b345f0fa28179ae4699d1b3a3445341ced
amr.9102@gmail.com <amr.9102@gmail.com>
2011-04-19 08:36:31 +0000

    Removed some empty lines

--- OJToolkit_3/src/com/OJToolkit_2/client/Services/LanguageServiceAsync.java
+++ OJToolkit_3/src/com/OJToolkit_2/client/Services/LanguageServiceAsync.java
@@ -6,9 +6,6 @@ import com.OJToolkit_2.client.ValueObjects.LanguageData;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface LanguageServiceAsync {
-
 	void getLanguages(AsyncCallback<ArrayList<LanguageData>> callback);
-
 	void addLanguages(AsyncCallback<Void> callback);
-
 }

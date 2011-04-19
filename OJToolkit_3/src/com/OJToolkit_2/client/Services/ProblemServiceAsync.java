a86fa03c6e2d0194532f133d69da8cd17d400085
amr.9102@gmail.com <amr.9102@gmail.com>
2011-04-19 08:50:47 +0000

    Removed empty lines

--- OJToolkit_3/src/com/OJToolkit_2/client/Services/ProblemServiceAsync.java
+++ OJToolkit_3/src/com/OJToolkit_2/client/Services/ProblemServiceAsync.java
@@ -6,11 +6,7 @@ import com.OJToolkit_2.client.ValueObjects.ProblemData;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface ProblemServiceAsync {
-
 	void getAllProblems(AsyncCallback<ArrayList<ProblemData>> callback);
-
 	void getPorblem(String probID, AsyncCallback<ProblemData> callback);
-
 	void addProblem(ProblemData probData, AsyncCallback<Void> callback);
-
 }

91a8df45c4447ee7109ac10444f5551adfbf8059
amr.9102@gmail.com <amr.9102@gmail.com>
2011-04-19 08:52:28 +0000

    Removed some empty lines

--- OJToolkit_3/src/com/OJToolkit_2/client/Services/SubmissionServiceAsync.java
+++ OJToolkit_3/src/com/OJToolkit_2/client/Services/SubmissionServiceAsync.java
@@ -5,15 +5,9 @@ import com.OJToolkit_2.client.ValueObjects.ProblemStatusData;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface SubmissionServiceAsync {
-
-
-
 	void submitCode(String prblmID, String code, String language,
 			AsyncCallback<Void> callback);
-
 	void getLastProblemStatus(AsyncCallback<ProblemStatusData> callback);
-
 	void saveSpojProblemtoDB(ProblemData problemData,
 			AsyncCallback<Void> callback);
-
 }

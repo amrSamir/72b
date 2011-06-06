2ece73d1ac0cea542d3c361de58f73fa7c3b3c04
Mohamed Magdi <moh_magdi@acm.org>
2011-06-06 18:11:21 +0000

    coder view [as List] change 1.codersPresenter 2.codersView

--- OJToolkit/src/com/OJToolkit/client/presenter/CodersPresenter.java
+++ OJToolkit/src/com/OJToolkit/client/presenter/CodersPresenter.java
@@ -4,40 +4,62 @@ import java.util.ArrayList;
 
 import com.OJToolkit.client.Services.CoderServiceAsync;
 import com.OJToolkit.client.ValueObjects.CoderData;
+import com.OJToolkit.client.ValueObjects.ProblemData;
 import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.user.cellview.client.AbstractHasData;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.Widget;
 
-
 public class CodersPresenter implements Presenter {
 
 	public interface Display {
-		void setCodersList(ArrayList<CoderData> coders);
+		AbstractHasData<CoderData> getTable();
+		void setCodersList(ArrayList<CoderData> coderlist);
+		void setNumberOfCoders(int numOfCoders);
+		void setPageStart(int pageStart);
+		// void setCodersList(ArrayList<CoderData> coders);
 		Widget asWidget();
 	}
 
 	private final Display display;
 	private final CoderServiceAsync coderService;
 	private final HandlerManager eventBus;
+	private final AbstractHasData<CoderData> codersTable;
+	private final ArrayList<CoderData> codersList;
+	private final int numOfCoders = 1;
+	private int pageStart = 0;
 
 	public CodersPresenter(CoderServiceAsync coderService,
-	        HandlerManager eventBus, final Display display) {
+			HandlerManager eventBus, final Display display) {
+		
 		this.coderService = coderService;
 		this.eventBus = eventBus;
 		this.display = display;
+		codersTable = this.display.getTable();
+		this.display.setNumberOfCoders(numOfCoders);
+		this.codersList = new ArrayList<CoderData>();
+		for (int i = 0; i <= numOfCoders; i++) {
+			codersList.add(null);
+		}
+		this.display.setCodersList(codersList);
+		fitchCoders() ;
+		this.display.setPageStart(pageStart);
 
+	}
+
+	public void fitchCoders() {
 		coderService.viewCoders(new AsyncCallback<ArrayList<CoderData>>() {
-
 			@Override
-			public void onSuccess(ArrayList<CoderData> result) {				
+			public void onSuccess(ArrayList<CoderData> result) {
 				display.setCodersList(result);
 			}
 
 			@Override
 			public void onFailure(Throwable caught) {
-				Window.alert("Failed to load coder Data!");				
+				Window.alert("Failed to load coder Data!\n" + "error : "
+						+ caught.getMessage());
 			}
 		});
 
--- OJToolkit/src/com/OJToolkit/client/view/CodersView.java
+++ OJToolkit/src/com/OJToolkit/client/view/CodersView.java
@@ -6,12 +6,21 @@ package com.OJToolkit.client.view;
 import java.util.ArrayList;
 
 import com.OJToolkit.client.ValueObjects.CoderData;
+import com.OJToolkit.client.ValueObjects.ProblemData;
 import com.OJToolkit.client.presenter.CodersPresenter;
+import com.google.gwt.user.cellview.client.AbstractHasData;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.SimplePager;
+import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.ListDataProvider;
 
 /**
  * @author 72B Apr 26, 2011
@@ -20,9 +29,54 @@ public class CodersView extends Composite implements CodersPresenter.Display {
 
 	AbsolutePanel absolutePanel;
 
+	int numberOfCoders;
+	int pageStart;
+	ArrayList<CoderData> codersList;
+	CellTable<CoderData> codersTable;
+	final ListDataProvider<CoderData> coderDataProvider;
+
 	public CodersView() {
-		absolutePanel = new AbsolutePanel();
-		initWidget(absolutePanel);
+		ScrollPanel ScrollCoders = new ScrollPanel() ;
+		ScrollCoders.setSize("100%", "100%") ;
+		initWidget(ScrollCoders);
+		
+		VerticalPanel vpanelCoder = new VerticalPanel() ;
+		ScrollCoders.add(vpanelCoder);
+		
+		coderDataProvider = new ListDataProvider<CoderData>();
+		TextColumn<CoderData> coderUserNameColumn = new TextColumn<CoderData>() {
+			@Override
+			public String getValue(CoderData coder) {
+				return coder.getUsername() ;
+			}
+		};
+		TextColumn<CoderData> coderEmailColumn = new TextColumn<CoderData>() {
+			@Override
+			public String getValue(CoderData coder) {
+				return coder.getEmail() ;
+			}
+		};
+		TextColumn<CoderData> coderSpojNameColumn = new TextColumn<CoderData>() {
+			@Override
+			public String getValue(CoderData coder) {
+				return coder.getSPOJUsername() ;
+			}
+		};
+
+		codersTable.addColumn(coderUserNameColumn,"coder name") ;
+		codersTable.addColumn(coderEmailColumn,"Email") ;
+		codersTable.addColumn(coderSpojNameColumn,"spoj user name") ;
+		codersTable.setRowCount(numberOfCoders,true) ;
+		
+		SimplePager coderTablePager = new SimplePager();
+		coderTablePager.setDisplay(codersTable);
+		coderTablePager.setPageSize(50);
+		
+		coderDataProvider.addDataDisplay(codersTable);
+		vpanelCoder.add(codersTable);
+		vpanelCoder.add(coderTablePager) ;
+		// absolutePanel = new AbsolutePanel();
+		// initWidget(absolutePanel);
 	}
 
 	/*
@@ -44,50 +98,71 @@ public class CodersView extends Composite implements CodersPresenter.Display {
 	 */
 	@Override
 	public void setCodersList(ArrayList<CoderData> coders) {
-		for (CoderData coder : coders) {
-			viewCoder(coder);
-		}
+//		if (coders.size() == 0)
+//			Window.alert("erorr!!");
+//		else
+//			Window.alert("coders size = " + coders.size());
+//		for (CoderData coder : coders) {
+//			viewCoder(coder);
+//		}
+		this.codersList = coders ;
+		coderDataProvider.setList(coders);
 	}
 
-	public void viewCoder(CoderData coder) {
+//	public void viewCoder(CoderData coder) {
+//
+//		Label lblUserData = new Label("User Data");
+//		absolutePanel.add(lblUserData);
+//
+//		Label lblNewLabel = new Label("UserID");
+//		absolutePanel.add(lblNewLabel);
+//
+//		TextBox txtUserID = new TextBox();
+//		txtUserID.setText(coder.getUserID().toString());
+//		absolutePanel.add(txtUserID);
+//
+//		Label lblUsername = new Label("Username");
+//		absolutePanel.add(lblUsername);
+//
+//		TextBox txtUserName = new TextBox();
+//		txtUserName.setText(coder.getUsername());
+//		absolutePanel.add(txtUserName);
+//
+//		Label lblEmail = new Label("Email");
+//		absolutePanel.add(lblEmail);
+//
+//		TextBox txtEmail = new TextBox();
+//		txtEmail.setText(coder.getEmail());
+//		absolutePanel.add(txtEmail);
+//
+//		Label lblSpojusername = new Label("SPOJUsername");
+//		absolutePanel.add(lblSpojusername);
+//
+//		TextBox txtSPOJUsername = new TextBox();
+//		txtSPOJUsername.setText(coder.getSPOJUsername());
+//		absolutePanel.add(txtSPOJUsername);
+//
+//		Label lblSpojpassword = new Label("SPOJPassword");
+//		absolutePanel.add(lblSpojpassword);
+//
+//		TextBox txtSpojPassword = new TextBox();
+//		txtSpojPassword.setText(coder.getSPOJPassword());
+//		absolutePanel.add(txtSpojPassword);
+//	}
 
-		Label lblUserData = new Label("User Data");
-		absolutePanel.add(lblUserData);
+	@Override
+	public AbstractHasData<CoderData> getTable() {
+		return codersTable ;
+	}
 
-		Label lblNewLabel = new Label("UserID");
-		absolutePanel.add(lblNewLabel);
+	@Override
+	public void setNumberOfCoders(int numOfCoders) {
+		this.numberOfCoders = numOfCoders ;
+	}
 
-		TextBox txtUserID = new TextBox();
-		txtUserID.setText(coder.getUserID().toString());
-		absolutePanel.add(txtUserID);
-
-		Label lblUsername = new Label("Username");
-		absolutePanel.add(lblUsername);
-
-		TextBox txtUserName = new TextBox();
-		txtUserName.setText(coder.getUsername());
-		absolutePanel.add(txtUserName);
-
-		Label lblEmail = new Label("Email");
-		absolutePanel.add(lblEmail);
-
-		TextBox txtEmail = new TextBox();
-		txtEmail.setText(coder.getEmail());
-		absolutePanel.add(txtEmail);
-
-		Label lblSpojusername = new Label("SPOJUsername");
-		absolutePanel.add(lblSpojusername);
-
-		TextBox txtSPOJUsername = new TextBox();
-		txtSPOJUsername.setText(coder.getSPOJUsername());
-		absolutePanel.add(txtSPOJUsername);
-
-		Label lblSpojpassword = new Label("SPOJPassword");
-		absolutePanel.add(lblSpojpassword);
-
-		TextBox txtSpojPassword = new TextBox();
-		txtSpojPassword.setText(coder.getSPOJPassword());
-		absolutePanel.add(txtSpojPassword);
+	@Override
+	public void setPageStart(int pageStart) {
+		this.pageStart = pageStart ;
 	}
 
 }

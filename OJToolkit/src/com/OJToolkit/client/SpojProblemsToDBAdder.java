08c5865728c70d00d07e1ba9c425589559c07d86
ahmedazraq@gmail.com <ahmedazraq@gmail.com@87d62ec8-b1a2-8f5d-c6ae-dd81f198c0ec>
2011-07-14 11:01:19 +0000

    init

--- OJToolkit/src/com/OJToolkit/client/SpojProblemsToDBAdder.java
+++ OJToolkit/src/com/OJToolkit/client/SpojProblemsToDBAdder.java
@@ -8,6 +8,9 @@ import com.OJToolkit.client.Services.SubmissionServiceAsync;
 import com.OJToolkit.client.ValueObjects.ProblemData;
 import com.OJToolkit.client.ValueObjects.ProblemTextData;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ResourceCallback;
+import com.google.gwt.resources.client.ResourceException;
+import com.google.gwt.resources.client.TextResource;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public class SpojProblemsToDBAdder {
@@ -21,53 +24,139 @@ public class SpojProblemsToDBAdder {
 
 	public SpojProblemsToDBAdder() {
 		System.out.println("ahmedazraq-SpojProblemsToDBAdder-Initializer");
-		// addSPOJProblems();
-		// addLanguages();
-		addSPOJProblemText();
+	addLanguages();
+		/*try {
+			addProblems("Timus");
+			addProblemsTextFromTextResource("Timus");
+			
+			//addProblems("SPOJ");
+			//addProblemsTextFromTextResource("SPOJ");
+		//	addProblems("UVA");
+	//		addProblemsTextFromTextResource("UVA");
+			
+			//addProblems("LiveArchive");
+		} catch (ResourceException e) {
+			System.out.println("CAn't load file");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+
+		/*
+		 * try {
+		 * addProblems("Timus");
+		 * } catch (ResourceException e) {
+		 * // TODO Auto-generated catch block
+		 * e.printStackTrace();
+		 * }
+		 */
+
 	}
 
-	private void addSPOJProblems() {
-		String problems = MyResource.INSTANCE.defaultText().getText();
+	private void addProblems(final String judgeType) throws ResourceException {
+		String problems = "";
+		if (judgeType.equals("Timus")) {
+			MyResource.INSTANCE.TimusProblems().getText(
+			        new ResourceCallback<TextResource>() {
+
+				        @Override
+				        public void onSuccess(TextResource resource) {
+					        System.out.println("Timus Problems");
+					        addProblemsHelper(judgeType, resource.getText());
+				        }
+
+				        @Override
+				        public void onError(ResourceException e) {
+					        // TODO Auto-generated method stub
+
+				        }
+			        });
+		}
+		else if (judgeType.equals("SPOJ")) {
+			MyResource.INSTANCE.SPOJProblems().getText(
+					new ResourceCallback<TextResource>() {
+						
+						@Override
+						public void onSuccess(TextResource resource) {
+							System.out.println("SPOJ Problems");
+							addProblemsHelper(judgeType, resource.getText());
+						}
+						
+						@Override
+						public void onError(ResourceException e) {
+							// TODO Auto-generated method stub
+							
+						}
+					});
+		}
+		else if (judgeType.equals("UVA")) {
+			MyResource.INSTANCE.UVAProblems().getText(
+					new ResourceCallback<TextResource>() {
+						
+						@Override
+						public void onSuccess(TextResource resource) {
+							System.out.println("UVA Problems");
+							addProblemsHelper(judgeType, resource.getText());
+						}
+						
+						@Override
+						public void onError(ResourceException e) {
+							// TODO Auto-generated method stub
+							
+						}
+					});
+		}
+		else if (judgeType.equals("LiveArchive")) {
+			MyResource.INSTANCE.LiveArchiveProblems().getText(
+					new ResourceCallback<TextResource>() {
+						
+						@Override
+						public void onSuccess(TextResource resource) {
+							System.out.println("LiveArchive Problems");
+							addProblemsHelper(judgeType, resource.getText());
+						}
+						
+						@Override
+						public void onError(ResourceException e) {
+							// TODO Auto-generated method stub
+							
+						}
+					});
+		}
+
+	}
+
+	public void addProblemsHelper(String judgeType, String problems) {
 		String[] linesArr = problems.split("\n");
 		ProblemData problem = new ProblemData();
 		String[] splitted;
-
 		for (int i = 0; i < linesArr.length; i++) {
 			splitted = linesArr[i].split(" \\| ");
 			problem.setProblemCode(splitted[0]);
 			problem.setProblemName(splitted[1]);
-
 			problem.setUrl(splitted[2].replaceAll("https", "http"));
-			problem.setOjType("SPOJ");
-
-			submissionService.saveSpojProblemtoDB(problem,
+			problem.setOjType(judgeType);
+			submissionService.saveProblemstoDB(problem,
 			        new AsyncCallback<Void>() {
-
 				        @Override
 				        public void onSuccess(Void result) {
 					        // TODO Auto-generated method stub
 					        success += 1;
-
 				        }
 
 				        @Override
 				        public void onFailure(Throwable caught) {
 					        failures = failures + 1;
 					        // TODO Auto-generated method stub
-
 				        }
 			        });
-
 		}
 		System.out.println(" Failures " + String.valueOf(failures)
 		        + " Success " + String.valueOf(success));
 	}
 
-	private void addSPOJProblemText() {
-		String problemsText = MyResource.INSTANCE.SPOJProblemText().getText();
+	private void addProblemsText(String judgeType, String problemsText) {
 		String[] problemsArr = problemsText
 		        .split("______________________________________________________");
-		// ProblemData problem = new ProblemData();
 		String[] problem;
 		String[] problemTextDivisionArr;
 		String isDividable = "";
@@ -78,8 +167,11 @@ public class SpojProblemsToDBAdder {
 		String outputConstraints = "";
 		String IOTestCases = "";
 		ProblemTextData problemTextData;
+		int i = 0;
 		try {
-			for (int i = 0; i < problemsArr.length; i++) {
+
+			for (i = 0; i < problemsArr.length; i++) {
+				// System.out.println(i);
 				problem = problemsArr[i].split("\\|{6}");
 				problemCode = problem[0].trim();
 				isDividable = problem[1].trim();
@@ -97,7 +189,7 @@ public class SpojProblemsToDBAdder {
 					outputConstraints = problemTextDivisionArr[2];
 					IOTestCases = problemTextDivisionArr[3];
 				}
-				problemTextData = new ProblemTextData("SPOJ", problemCode,
+				problemTextData = new ProblemTextData(judgeType, problemCode,
 				        isDividable, fullText, problemStatement,
 				        inputConstraints, outputConstraints, IOTestCases);
 
@@ -114,14 +206,340 @@ public class SpojProblemsToDBAdder {
 				        });
 
 			}
+			System.out.println("Finished el7");
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
+			System.out.println("EXCEEEEEEEEEEEEPTION");
+			System.out.println(problemsArr[i - 1].substring(0, 10));
+			System.out.println(problemsArr[i].substring(0, 10));
+			System.out.print("PROBLEM CODE: ");
 			System.out.println(problemCode);
 			System.out.println("problem state" + problemStatement);
 			System.out.println("input Constraint" + inputConstraints);
 			System.out.println("output constraint" + outputConstraints);
 			System.out.println("io test cases" + IOTestCases);
 		}
+
+	}
+
+	private void uva2() throws ResourceException {
+		MyResource.INSTANCE.UVAProblemText2().getText(
+		        new ResourceCallback<TextResource>() {
+
+			        @Override
+			        public void onSuccess(TextResource resource) {
+				        System.out.println("uva2");
+				        addProblemsText("UVA", resource.getText());
+				        try {
+					        uva3();
+				        } catch (ResourceException e) {
+					        // TODO Auto-generated catch block
+					        e.printStackTrace();
+				        }
+			        }
+
+			        @Override
+			        public void onError(ResourceException e) {
+				        // TODO Auto-generated method stub
+
+			        }
+		        });
+	}
+
+	private void uva3() throws ResourceException {
+		MyResource.INSTANCE.UVAProblemText3().getText(
+		        new ResourceCallback<TextResource>() {
+
+			        @Override
+			        public void onSuccess(TextResource resource) {
+				        System.out.println("uva3");
+				        addProblemsText("UVA", resource.getText());
+				        try {
+					        uva4();
+				        } catch (ResourceException e) {
+					        // TODO Auto-generated catch block
+					        e.printStackTrace();
+				        }
+			        }
+
+			        @Override
+			        public void onError(ResourceException e) {
+				        // TODO Auto-generated method stub
+
+			        }
+		        });
+	}
+
+	private void uva4() throws ResourceException {
+		MyResource.INSTANCE.UVAProblemText4().getText(
+		        new ResourceCallback<TextResource>() {
+
+			        @Override
+			        public void onSuccess(TextResource resource) {
+				        System.out.println("uva4");
+				        addProblemsText("UVA", resource.getText());
+				        try {
+					        uva5();
+				        } catch (ResourceException e) {
+					        // TODO Auto-generated catch block
+					        e.printStackTrace();
+				        }
+			        }
+
+			        @Override
+			        public void onError(ResourceException e) {
+				        // TODO Auto-generated method stub
+
+			        }
+		        });
+	}
+
+	private void uva5() throws ResourceException {
+		MyResource.INSTANCE.UVAProblemText5().getText(
+		        new ResourceCallback<TextResource>() {
+
+			        @Override
+			        public void onSuccess(TextResource resource) {
+				        System.out.println("uva5");
+				        addProblemsText("UVA", resource.getText());
+				        try {
+					        uva6();
+				        } catch (ResourceException e) {
+					        // TODO Auto-generated catch block
+					        e.printStackTrace();
+				        }
+			        }
+
+			        @Override
+			        public void onError(ResourceException e) {
+				        // TODO Auto-generated method stub
+
+			        }
+		        });
+	}
+
+	private void uva6() throws ResourceException {
+		MyResource.INSTANCE.UVAProblemText6().getText(
+		        new ResourceCallback<TextResource>() {
+
+			        @Override
+			        public void onSuccess(TextResource resource) {
+				        System.out.println("uva6");
+				        addProblemsText("UVA", resource.getText());
+				        try {
+					        uva7();
+				        } catch (ResourceException e) {
+					        // TODO Auto-generated catch block
+					        e.printStackTrace();
+				        }
+			        }
+
+			        @Override
+			        public void onError(ResourceException e) {
+				        // TODO Auto-generated method stub
+
+			        }
+		        });
+	}
+
+	private void uva7() throws ResourceException {
+		MyResource.INSTANCE.UVAProblemText7().getText(
+		        new ResourceCallback<TextResource>() {
+
+			        @Override
+			        public void onSuccess(TextResource resource) {
+				        System.out.println("uva7");
+				        addProblemsText("UVA", resource.getText());
+				        try {
+					        uva8();
+				        } catch (ResourceException e) {
+					        // TODO Auto-generated catch block
+					        e.printStackTrace();
+				        }
+			        }
+
+			        @Override
+			        public void onError(ResourceException e) {
+				        // TODO Auto-generated method stub
+
+			        }
+		        });
+	}
+
+	private void uva8() throws ResourceException {
+		MyResource.INSTANCE.UVAProblemText8().getText(
+		        new ResourceCallback<TextResource>() {
+
+			        @Override
+			        public void onSuccess(TextResource resource) {
+				        System.out.println("uva8");
+				        addProblemsText("UVA", resource.getText());
+				        System.out.println("el7777777777 done");
+			        }
+
+			        @Override
+			        public void onError(ResourceException e) {
+				        // TODO Auto-generated method stub
+
+			        }
+		        });
+	}
+
+	private void addProblemsTextFromTextResource(String judgeType)
+	        throws ResourceException {
+
+		if (judgeType.equals("SPOJ")) {
+			MyResource.INSTANCE.SPOJProblemText1().getText(
+			        new ResourceCallback<TextResource>() {
+
+				        @Override
+				        public void onSuccess(TextResource resource) {
+					        System.out.println("SPOJ1");
+					        addProblemsText("SPOJ", resource.getText());
+							try {
+	                            MyResource.INSTANCE.SPOJProblemText2().getText(
+	                                    new ResourceCallback<TextResource>() {
+
+	                            	        @Override
+	                            	        public void onSuccess(TextResource resource) {
+	                            		        System.out.println("SPOJ2");
+	                            		        addProblemsText("SPOJ", resource.getText());
+
+	                            	        }
+
+	                            	        @Override
+	                            	        public void onError(ResourceException e) {
+	                            		        // TODO Auto-generated method stub
+
+	                            	        }
+	                                    });
+                            } catch (ResourceException e) {
+	                            // TODO Auto-generated catch block
+	                            e.printStackTrace();
+                            }
+
+				        }
+
+				        @Override
+				        public void onError(ResourceException e) {
+					        // TODO Auto-generated method stub
+
+				        }
+			        });
+		} else if (judgeType.equals("Timus")) {
+			MyResource.INSTANCE.TimusProblemText().getText(
+			        new ResourceCallback<TextResource>() {
+
+				        @Override
+				        public void onSuccess(TextResource resource) {
+					        System.out.println("timus");
+					        addProblemsText("Timus", resource.getText());
+
+				        }
+
+				        @Override
+				        public void onError(ResourceException e) {
+					        // TODO Auto-generated method stub
+
+				        }
+			        });
+		} else if (judgeType.equals("UVA")) {
+			MyResource.INSTANCE.UVAProblemText1().getText(
+			        new ResourceCallback<TextResource>() {
+
+				        @Override
+				        public void onSuccess(TextResource resource) {
+					        System.out.println("uva1");
+					        addProblemsText("UVA", resource.getText());
+					        try {
+						        uva2();
+					        } catch (ResourceException e) {
+						        // TODO Auto-generated catch block
+						        System.out.println("uva 2 file exception");
+					        }
+				        }
+
+				        @Override
+				        public void onError(ResourceException e) {
+					        // TODO Auto-generated method stub
+
+				        }
+			        });
+			/*
+			 * MyResource.INSTANCE.UVAProblemText3().getText(
+			 * new ResourceCallback<TextResource>() {
+			 * @Override
+			 * public void onSuccess(TextResource resource) {
+			 * System.out.println("uva3");
+			 * addProblemsText("UVA", resource.getText());
+			 * }
+			 * @Override
+			 * public void onError(ResourceException e) {
+			 * // TODO Auto-generated method stub
+			 * }
+			 * });
+			 * MyResource.INSTANCE.UVAProblemText4().getText(
+			 * new ResourceCallback<TextResource>() {
+			 * @Override
+			 * public void onSuccess(TextResource resource) {
+			 * System.out.println("uva4");
+			 * addProblemsText("UVA", resource.getText());
+			 * }
+			 * @Override
+			 * public void onError(ResourceException e) {
+			 * // TODO Auto-generated method stub
+			 * }
+			 * });
+			 * MyResource.INSTANCE.UVAProblemText5().getText(
+			 * new ResourceCallback<TextResource>() {
+			 * @Override
+			 * public void onSuccess(TextResource resource) {
+			 * System.out.println("uva5");
+			 * addProblemsText("UVA", resource.getText());
+			 * }
+			 * @Override
+			 * public void onError(ResourceException e) {
+			 * // TODO Auto-generated method stub
+			 * }
+			 * });
+			 * MyResource.INSTANCE.UVAProblemText6().getText(
+			 * new ResourceCallback<TextResource>() {
+			 * @Override
+			 * public void onSuccess(TextResource resource) {
+			 * System.out.println("uva6");
+			 * addProblemsText("UVA", resource.getText());
+			 * }
+			 * @Override
+			 * public void onError(ResourceException e) {
+			 * // TODO Auto-generated method stub
+			 * }
+			 * });
+			 * MyResource.INSTANCE.UVAProblemText7().getText(
+			 * new ResourceCallback<TextResource>() {
+			 * @Override
+			 * public void onSuccess(TextResource resource) {
+			 * System.out.println("uva7");
+			 * addProblemsText("UVA", resource.getText());
+			 * }
+			 * @Override
+			 * public void onError(ResourceException e) {
+			 * // TODO Auto-generated method stub
+			 * }
+			 * });
+			 * MyResource.INSTANCE.UVAProblemText8().getText(
+			 * new ResourceCallback<TextResource>() {
+			 * @Override
+			 * public void onSuccess(TextResource resource) {
+			 * System.out.println("uva8");
+			 * addProblemsText("UVA", resource.getText());
+			 * }
+			 * @Override
+			 * public void onError(ResourceException e) {
+			 * // TODO Auto-generated method stub
+			 * }
+			 * });
+			 */
+		}
+
 	}
 
 	private void addLanguages() {
